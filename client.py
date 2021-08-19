735a77c0c357953994d1b2fce378fe332dc12ae4
lnSoMnia <30279579+lnSoMnia@users.noreply.github.com>
2021-08-19 09:35:38 +0800

    Update client.py

--- client.py
+++ client.py
@@ -31,9 +31,8 @@ class client(object):
 
 
 class ClientsGroup(object):
-    def __init__(self, dataSetName, isIID, numOfClients, dev):
+    def __init__(self, dataSetName, numOfClients, dev):
         self.data_set_name = dataSetName
-        self.is_iid = isIID
         self.num_of_clients = numOfClients
         self.dev = dev
         self.clients_set = {}
@@ -43,17 +42,22 @@ class ClientsGroup(object):
         self.dataSetBalanceAllocation()
 
     def dataSetBalanceAllocation(self):
-        UTKDataSet = GetDataSet(self.data_set_name, self.is_iid)
+        UTKDataSet = GetDataSet(self.data_set_name)
 
-        test_data = torch.tensor(UTKDataSet.test_data)
+        test_data = UTKDataSet.test_data.clone()
         test_label = torch.argmax(torch.tensor(UTKDataSet.test_label), dim=1)
         self.test_data_loader = DataLoader(TensorDataset( test_data, test_label), batch_size=100, shuffle=False)
 
         train_data = UTKDataSet.train_data
         train_label = UTKDataSet.train_label
-
-        shard_size = UTKDataSet.train_data_size // self.num_of_clients // 2
-        shards_id = np.random.permutation(UTKDataSet.train_data_size // shard_size)
+        
+        #X_col=np.size(X,1)
+        #print(UTKDataSet.train_data)
+        #print(UTKDataSet.train_data.shape)
+        print(len(UTKDataSet.train_data))
+        #shard_size = UTKDataSet.train_data_size // self.num_of_clients // 2
+        shard_size = 6000 // self.num_of_clients // 2
+        shards_id = np.random.permutation(6000 // shard_size)
         for i in range(self.num_of_clients):
             shards_id1 = shards_id[i * 2]
             shards_id2 = shards_id[i * 2 + 1]
@@ -67,8 +71,7 @@ class ClientsGroup(object):
             self.clients_set['client{}'.format(i)] = someone
 
 if __name__=="__main__":
-    MyClients = ClientsGroup('crop_part1', True, 100, 1)
-    print(MyClients.clients_set['client10'].train_ds[0:100])
-    print(MyClients.clients_set['client11'].train_ds[400:500])
-
-
+    MyClients = ClientsGroup('crop_part1', 100, 1)
+    
+    #print(MyClients.clients_set['client10'].train_ds[0:100])
+    #print(MyClients.clients_set['client11'].train_ds[400:500])
